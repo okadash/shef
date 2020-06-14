e6d1ef5ea65bd8dc2e8b6c98873ca54853f26776
Okadarien Saru <okadas@dev.sysnk.net>
2020-06-15 00:13:32 +0900

    [bugfix] installer fixed

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ command -v git || { echo git not found; exit 1; }
 
 git clone https://github.com/okadash/shelib .tmp.core
 cd .tmp.core 
-./install.sh
+./install.sh && cd .. || { echo shelib installation failure; exit 1;}
+
 test -d ~/.shef/lib/shef && { echo shef is allready installed; exit 1; }
 cp -av lib ~/.shef/lib/shef
 ./bin/shef install
