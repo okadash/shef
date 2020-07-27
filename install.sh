10137475681ef3f3c0852a7ad8b3bb3dee6bc62b
Okadarien Saru <okadas@dev.sysnk.net>
2020-07-27 21:58:07 +0900

    [bugfix] install script fixed to use valid directories

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
-#!/bin/sh
+#!/bin/sh -e
 
 command -v git || { echo git not found; exit 1; }
 
-git clone https://github.com/okadash/shelib .tmp.core
-cd .tmp.core 
-./install.sh && cd .. || { echo shelib installation failure; exit 1;}
+test -d ~/.shef/lib/core || {
+  git clone https://github.com/okadash/shelib .tmp.core
+  cd .tmp.core 
+  ./install.sh && cd .. || { echo shelib installation failure; exit 1;}
+  cd ..
+}
 
-test -d ~/.shef/lib/shef && { echo shef is allready installed; exit 1; }
-cp -av lib ~/.shef/lib/shef
-./bin/shef install
+test -d ~/.shef/lib/shef && { echo shef is allready installed; exit 1; } || {
+  cp -av lib ~/.shef/lib/shef
+  ./bin/shef install || cp -av bin/shef ~/.shef/bin/
+}
