5512e21dbce90c6a718ce68a10cb43ce011e14b0
Okadarien Saru <okadas@dev.sysnk.net>
2020-07-27 22:46:40 +0900

    [experimental] shef package listing and searching function

--- install.sh
+++ install.sh
@@ -1,15 +1,21 @@
 #!/bin/sh -e
 
+SHELIB_ROOT=~/.shef
+
 command -v git || { echo git not found; exit 1; }
 
-test -d ~/.shef/lib/core || {
+test -d $SHELIB_ROOT/lib/core || {
   git clone https://github.com/okadash/shelib .tmp.core
   cd .tmp.core 
   ./install.sh && cd .. || { echo shelib installation failure; exit 1;}
   cd ..
 }
 
-test -d ~/.shef/lib/shef && { echo shef is allready installed; exit 1; } || {
-  cp -av lib ~/.shef/lib/shef
-  ./bin/shef install || cp -av bin/shef ~/.shef/bin/
+test -d $SHELIB_ROOT/db && { echo shefpkg database exists; } || {
+  cp -av db $SHELIB_ROOT/
+}
+
+test -d $SHELIB_ROOT/lib/shef && { echo shef is allready installed; exit 1; } || {
+  cp -av lib $SHELIB_ROOT/lib/shef
+  ./bin/shef install || cp -av bin/shef $SHELIB_ROOT/bin/
 }
